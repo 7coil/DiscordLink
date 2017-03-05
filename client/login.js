e0529e8a83679ad163f1b8156d4ad59400f00472
7coil <leondrolio@gmail.com>
2017-03-05 19:02:52 +0000

    Fixing URI component for the good of all of us

--- client/login.js
+++ client/login.js
@@ -1,5 +1,12 @@
 var params={};location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(s,k,v){params[k]=v})
+var textbox = document.getElementById("username");
 
 if (params["error"]) {
 	$("#error").html("Error! " + decodeURIComponent(params["error"]));
 }
+
+textbox.addEventListener("keydown", function(event) {
+	if (event.keyCode == 13 && textbox.value.length > 0) {
+		$(location).attr('href', '/client/?username=' + encodeURIComponent(textbox.value));
+	}
+}, false);
