00659de1d70853be0f952541b3c7c229df8134c6
7coil <leondrolio@gmail.com>
2017-03-05 00:07:28 +0000

    ENDING BRACKETS ALSO GOOD!

--- client/client.js
+++ client/client.js
@@ -25,7 +25,7 @@ socket.on("message", function (data){
 socket.on("url", function(data) {
 	messages.innerHTML += escapeHtml(data.username + "@" + data.source + "Attachment:") + "<a href='" + element.url.replace(/'/g, "%27").replace(/"/g, "%22") + "'>" + escapeHtml(element.url) + "</a>";
 	$("#bkmchat").scrollTop($("#bkmchat").height());
-}
+});
 
 socket.on("connect", function () {
 	user = user || prompt("Please insert a username.");
