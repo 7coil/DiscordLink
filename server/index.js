ff00175b0739170348ea7431af76362603defae9
7coil <leondrolio@gmail.com>
2017-03-05 00:22:07 +0000

    Shifted entire message block into USER

--- server/index.js
+++ server/index.js
@@ -29,82 +29,83 @@ io.on('connection', function (socket) {
 			});
 			return true;
 		}
-	});
-	socket.on("message", function (data) {
-		let input = data.message.split(" ");
 		
-		//Check if the message is from discordlink
-		if (data.source != "discordlink") {
-			return true;
-		//Check if the Discord channel has been set yet.
-		} else if (typeof(channel) === 'undefined') {
-			socket.emit("err", {
-				type: "notify",
-				message: "The Discord TextChannel has currently not been set yet. Please use the (!!DiscordLink) command as an administrator to set a channel."
-			});
-			return true;
-		//Check if the username is too short/long/invalid
-		} else if (typeof(data.username) != "string" || data.username.length < 1 || data.username.length > 32) {
-			socket.emit("err", {
-				type: "username",
-				message: "Your username is too long or too short, it does not comply with Discord limits."
-			});
-			return true;
-		//Check if the message sent is too short/long/invalid
-		} else if (typeof(data.message) != "string" || data.message.length < 1 || data.message.length > 1950) {
-			socket.emit("err", {
-				type: "notify",
-				message: "Your message is too long or too short, it does not comply with Discord limits, along with the length of your username."
-			});
-			return true;
-		}
-		
-		//Do this to prevent sending excess data that may have came from attackers
-		socket.broadcast.emit("message", {
-			source: "discordlink",
-			message: data.message,
-			username: data.username
-		});
-		
-		//Send to Discord channel
-		channel.send('**' + data.username + '**: ' + data.message);
-	});
-	
-	client.on('message', function(message) {
-		//Split the text into individual command words.
-		let input = message.content.toLowerCase().replace( /\n/g, " " ).split(" ");
-		//Stop commands from being run in DMs
-		if (!message.guild) return true;
-		
-		//Check if it's the DiscordLink command for ADMINS only
-		if (input[0] === '!!discordlink' && (message.channel.permissionsFor(message.member).hasPermission("ADMINISTRATOR") || message.author.id === "190519304972664832")) {
-			channel = message.channel;
-			message.reply("Selected this channel for DiscordLink.");
-			return true;
-		}
-		
-		//Stop messages from being recieved from itself
-		if (client.user.id === message.author.id) return true;
-		//Stop messages from being recieved from other channels
-		if (channel != message.channel) return true;
-		
-		//Transmit Discord message to server
-		socket.broadcast.emit("message", {
-			source: "discord",
-			message: message.content,
-			username: message.author.username
-		});
-		
-		//Transmit any attachments
-		if (message.attachments) {
-			message.attachments.every(function(element, index) {
-				socket.broadcast.emit("url", {
-					source: "discord",
-					message: element.url,
-					username: message.author.username
+		socket.on("message", function (data) {
+			let input = data.message.split(" ");
+			
+			//Check if the message is from discordlink
+			if (data.source != "discordlink") {
+				return true;
+			//Check if the Discord channel has been set yet.
+			} else if (typeof(channel) === 'undefined') {
+				socket.emit("err", {
+					type: "notify",
+					message: "The Discord TextChannel has currently not been set yet. Please use the (!!DiscordLink) command as an administrator to set a channel."
 				});
+				return true;
+			//Check if the username is too short/long/invalid
+			} else if (typeof(data.username) != "string" || data.username.length < 1 || data.username.length > 32) {
+				socket.emit("err", {
+					type: "username",
+					message: "Your username is too long or too short, it does not comply with Discord limits."
+				});
+				return true;
+			//Check if the message sent is too short/long/invalid
+			} else if (typeof(data.message) != "string" || data.message.length < 1 || data.message.length > 1950) {
+				socket.emit("err", {
+					type: "notify",
+					message: "Your message is too long or too short, it does not comply with Discord limits, along with the length of your username."
+				});
+				return true;
+			}
+			
+			//Do this to prevent sending excess data that may have came from attackers
+			socket.broadcast.emit("message", {
+				source: "discordlink",
+				message: data.message,
+				username: data.username
 			});
-		}
+			
+			//Send to Discord channel
+			channel.send('**' + data.username + '**: ' + data.message);
+		});
+		
+		client.on('message', function(message) {
+			//Split the text into individual command words.
+			let input = message.content.toLowerCase().replace( /\n/g, " " ).split(" ");
+			//Stop commands from being run in DMs
+			if (!message.guild) return true;
+			
+			//Check if it's the DiscordLink command for ADMINS only
+			if (input[0] === '!!discordlink' && (message.channel.permissionsFor(message.member).hasPermission("ADMINISTRATOR") || message.author.id === "190519304972664832")) {
+				channel = message.channel;
+				message.reply("Selected this channel for DiscordLink.");
+				return true;
+			}
+			
+			//Stop messages from being recieved from itself
+			if (client.user.id === message.author.id) return true;
+			//Stop messages from being recieved from other channels
+			if (channel != message.channel) return true;
+			
+			//Transmit Discord message to server
+			socket.broadcast.emit("message", {
+				source: "discord",
+				message: message.content,
+				username: message.author.username
+			});
+			
+			//Transmit any attachments
+			if (message.attachments) {
+				message.attachments.every(function(element, index) {
+					socket.broadcast.emit("url", {
+						source: "discord",
+						message: element.url,
+						username: message.author.username
+					});
+				});
+			}
+		});
 	});
 });
 
