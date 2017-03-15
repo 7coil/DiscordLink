63fe811133652f2af3f2973671a00ff071afac43
7coil <leondrolio@gmail.com>
2017-03-15 16:25:03 +0000

    console dir POST requests to POST

--- server/index.js
+++ server/index.js
@@ -25,7 +25,6 @@ client.on('ready', function() {
 
 io.on('connection', function (socket) {
 	socket.on("user", function (data) {
-		console.dir(data);
 		//Check if the username is too short/long/invalid
 		if (typeof(data.username) != "string" || data.username.length < 1 || data.username.length > 32) {
 			socket.emit("err", {
@@ -40,9 +39,6 @@ io.on('connection', function (socket) {
 		});
 	});
 	socket.on("message", function (data) {
-		console.dir(data);
-		
-		
 		//Check if the Discord channel has been set yet.
 		if (typeof(channel) === 'undefined') {
 			socket.emit("system", {
@@ -71,7 +67,8 @@ io.on('connection', function (socket) {
 			});
 			return true;
 		}
-		let input = data.message.split(" ");		//Do this to prevent sending excess data that may have came from attackers
+		
+		//Do this to prevent sending excess data that may have came from attackers
 		socket.broadcast.emit("message", {
 			source: data.source,
 			message: data.message,
@@ -86,39 +83,49 @@ io.on('connection', function (socket) {
 server.listen(process.env.PORT || 8080);
 
 client.on('message', function(message) {
-		//Split the text into individual command words.
-		let input = message.content.toLowerCase().replace( /\n/g, " " ).split(" ");
-		//Stop commands from being run in DMs
-		if (!message.guild) return true;
-		
-		//Check if it's the DiscordLink command for ADMINS only
-		if (input[0] === '!!discordlink' && (message.channel.permissionsFor(message.member).hasPermission("ADMINISTRATOR") || message.author.id === "190519304972664832" || message.author.id === "114049019227013128")) {
-			channel = message.channel;
-			fs.writeFile('channel.txt', message.channel.id);
-			message.reply("Selected this channel for DiscordLink.");
-			return true;
-		}
-		
-		//Stop messages from being recieved from itself
-		if (client.user.id === message.author.id) return true;
-		//Stop messages from being recieved from other channels
-		if (channel != message.channel) return true;
-		
-		//Transmit Discord message to server
-		io.sockets.emit("message", {
-			source: "discord",
-			message: message.content,
-			username: message.author.username
-		});
-		
-		//Transmit any attachments
-		if (message.attachments) {
-			message.attachments.every(function(element, index) {
-				io.sockets.emit("url", {
-					source: "discord",
-					message: element.url,
-					username: message.author.username
-				});
-			});
-		}
+	//Split the text into individual command words.
+	let input = message.content.toLowerCase().replace( /\n/g, " " ).split(" ");
+	//Stop commands from being run in DMs
+	if (!message.guild) return true;
+	
+	//Check if it's the DiscordLink command for ADMINS only
+	if (input[0] === '!!discordlink' && (message.channel.permissionsFor(message.member).hasPermission("ADMINISTRATOR") || message.author.id === "190519304972664832" || message.author.id === "114049019227013128")) {
+		channel = message.channel;
+		fs.writeFile('channel.txt', message.channel.id);
+		message.reply("Selected this channel for DiscordLink.");
+		return true;
+	}
+	
+	//Stop messages from being recieved from itself
+	if (client.user.id === message.author.id) return true;
+	//Stop messages from being recieved from other channels
+	if (channel != message.channel) return true;
+	
+	//Transmit Discord message to server
+	io.sockets.emit("message", {
+		source: "discord",
+		message: message.content,
+		username: message.author.username
 	});
+	
+	//Transmit any attachments
+	if (message.attachments) {
+		message.attachments.every(function(element, index) {
+			io.sockets.emit("url", {
+				source: "discord",
+				message: element.url,
+				username: message.author.username
+			});
+		});
+	}
+});
+
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.post("/POST/", function(req, res) {
+	console.dir(req.body);
+});
